86f87416380ef2e92b1a052ba63df046447b088c
MNM <tc-mnm@hotmail.com>
2025-05-22 04:12:28 +0330

    Create nuclei.go

---
+++ pkg/recon/nuclei.go
@@ -0,0 +1,18 @@
+```go
+package recon
+
+import (
+	"os"
+	"os/exec"
+	"sparky/pkg/config"
+)
+
+func ScanNuclei(inputFile, outputFile string, cfg *config.Config) error {
+	cmd := exec.Command("nuclei", "-l", inputFile, "-t", cfg.Paths.NucleiTemplates)
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(outputFile, out, 0644)
+}
+```
